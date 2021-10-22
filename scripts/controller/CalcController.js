038001f76664a6acac0bfeb94226f0203bfa5a3f
Ivanildo Ferreira <isivanildo@hotmail.com>
2021-10-22 11:33:01 -0300

    Configuração da data e hora do display da Calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,31 +1,59 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = "0";
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+
         this._currentDate;
         this.initialize();
     }
 
     initialize() {
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();
 
-        displayCalcEl.innerHTML = "12345";
-        dateEl.innerHTML = "22/10/2021";
-        timeEl.innerHTML = "10:37"
+        setInterval(()=>{
+            this.setDisplayDateTime();
+        }, 1000)
+
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day:'2-digit',
+            month:'long',
+            year:'numeric'
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value) {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate() {
-        return this._currentDate;
+        return new Date();
     }
 
     set currentDate(value) {
