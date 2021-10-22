e7ff5c046908507e58af678120f6de00f3517359
Ivanildo Ferreira <isivanildo@hotmail.com>
2021-10-22 10:39:54 -0300

    Manipulando o DOM do display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,6 +3,17 @@ class CalcController {
     constructor() {
         this._displayCalc = "0";
         this._currentDate;
+        this.initialize();
+    }
+
+    initialize() {
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "12345";
+        dateEl.innerHTML = "22/10/2021";
+        timeEl.innerHTML = "10:37"
     }
 
     get displayCalc() {
